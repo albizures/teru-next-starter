c683b27383738a81ff93fe093da46c59bf162474
Jose Albizures <albizures3601@gmail.com>
2020-05-09 14:12:07 +0300

    fix(files): use the correct filenames

--- teru.starter.js
+++ teru.starter.js
@@ -8,40 +8,32 @@ module.exports = {
 		email: {},
 		tailwind: {
 			defaultValue: true,
-			message:
-				'Do you want to use tailwind?',
+			message: 'Do you want to use tailwind?',
 		},
 		typescript: {
 			defaultValue: true,
-			message:
-				'Do you want to use typescript?',
+			message: 'Do you want to use typescript?',
 		},
 	},
 	files: [
 		'LICENSE',
 		'package.json',
-		'src/pages/_app.js',
-		'src/pages/_app.tsx',
 		{
 			onlyWhen: { typescript: true },
 			filename: [
-				'index.tsx',
-				'_app.tsx',
+				'src/pages/index.tsx',
+				'src/pages/_app.tsx',
 				'tsconfig.json',
 				'next-env.d.ts',
 			],
 		},
 		{
 			onlyWhen: { typescript: false },
-			filename: ['index.js', '_app.js'],
+			filename: ['src/pages/index.js', 'src/pages/_app.js'],
 		},
 		{
 			onlyWhen: { tailwind: true },
-			filename: [
-				'postcss.config.js',
-				'tailwind.config.js',
-				'src/styles.css',
-			],
+			filename: ['postcss.config.js', 'tailwind.config.js', 'src/styles.css'],
 		},
 	],
 };
